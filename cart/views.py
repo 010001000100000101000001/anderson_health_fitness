4e16b65870ad5a2b4c557fa52563d077cc04f97d
David Anderson <davidanderson@live.ie>
2024-09-06 13:28:48 +0000

    Added add_to_cart view to handle adding gear items to the cart and updated cart/urls.py with the corresponding URL path

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from workout_gear.models import GearItem
 
 # Create your views here.
 
@@ -6,3 +7,28 @@ def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """
+    Add a specified quantity of a gear item to the shopping cart.
+    """
+    quantity = int(request.POST.get('quantity', 1))
+    redirect_url = request.POST.get('redirect_url', '/')
+
+    gear_item = get_object_or_404(GearItem, pk=item_id)
+    cart = request.session.get('cart', {})
+
+    # Update cart contents
+    if item_id in cart:
+        cart[item_id]['quantity'] += quantity
+    else:
+        cart[item_id] = {
+            'quantity': quantity,
+            'name': gear_item.name,
+            'price': str(gear_item.cost),
+            'image_url': gear_item.image_file.url if gear_item.image_file else None,
+        }
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
