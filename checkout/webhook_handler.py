9489e2372b28cf40d64a6a02133404f67fbc035b
David Anderson <davidanderson@live.ie>
2024-09-30 13:29:41 +0000

    Added user profile update in Stripe webhook handler to save shipping information when save_info is checked and imported UserProfile model

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django_countries import countries
 from .models import Order, OrderLineItem
 from workout_gear.models import GearItem
+from profiles.models import UserProfile
 
 import json
 import time
@@ -43,6 +44,21 @@ class StripeWH_Handler:
         country_name = shipping_details.address.country
         country_code = dict(countries).get(country_name, country_name)
 
+        # Update Profile information if save_info was checked
+        profile- None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -77,6 +93,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=country_code,  # Use the 2-character country code
