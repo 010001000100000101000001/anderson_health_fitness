47ff7d91a4def12eb30e8fc71a61095dccce4597
David Anderson <davidanderson@live.ie>
2024-10-03 10:56:41 +0000

    Set Debug foir development

--- anderson_health_fitness/settings.py
+++ anderson_health_fitness/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')  # Fallback to 'default-secret-key' if not set
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-01000100010-andersonhea-0xa2lsw1mgo.ws.codeinstitute-ide.net',
