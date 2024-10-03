4ecd1c6764e4bb55389cfc53bd82f975d5cb31c3
David Anderson <davidanderson@live.ie>
2024-10-03 09:59:42 +0000

    Added ProductForm

--- workout_gear/forms.py
+++ workout_gear/forms.py
@@ -1,7 +1,24 @@
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from .models import GearCategory, GearItem
 from .models import ProductReview
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = GearItem
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = GearCategory.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
+
 
 class ProductReviewForm(forms.ModelForm):
     """
