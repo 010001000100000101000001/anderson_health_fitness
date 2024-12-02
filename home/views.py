4bd1cf22e2f81c28033dcb89394b33c8c6039482
David Anderson <davidanderson@live.ie>
2024-12-02 19:00:42 +0000

    Removed required field from template to fix error message when inputting a legit email and run views.py through CI python linter

--- home/views.py
+++ home/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import NewsletterSignupForm
 
+
 def index(request):
     """ A view to return the index page """
     if request.method == "POST":
         form = NewsletterSignupForm(request.POST)
         if form.is_valid():
-            # Simulate saving the email or sending it to a mailing list service
+            # Simulate saving the email or sending it to a mailing list service (mailchimp)
             email = form.cleaned_data['email']
-            messages.success(request, f"Thank you for signing up with {email}!")
+            messages.success(
+                request, f"Thank you for signing up with {email}!")
         else:
             messages.error(request, "Please enter a valid email address.")
     else:
@@ -20,12 +22,11 @@ def index(request):
     }
     return render(request, 'home/index.html', context)
 
+
 def subscribe(request):
-    """ A view to handle newsletter subscription """
     if request.method == "POST":
         form = NewsletterSignupForm(request.POST)
         if form.is_valid():
-            # Simulate saving the email or sending it to a mailing list service
             messages.success(request, "Thank you for subscribing!")
         else:
             messages.error(request, "Please enter a valid email address.")
