e20fe7335cd14fdafe93f70169a5c8439b6c4227
David Anderson <davidanderson@live.ie>
2024-12-02 17:36:06 +0000

    Update base.html to include toast notifications

--- home/views.py
+++ home/views.py
@@ -1,24 +1,7 @@
 from django.shortcuts import render
-from django.contrib import messages
-from .forms import NewsletterSignupForm
 
 
 def index(request):
     """ A view to return the index page """
-    if request.method == "POST":
-        form = NewsletterSignupForm(request.POST)
-        if form.is_valid():
-            # Simulated success (no Mailchimp integration)
-            email = form.cleaned_data['email']
-            messages.success(
-                request, f"Thank you for signing up with {email}!"
-                )
-        else:
-            messages.error(request, "Please enter a valid email address.")
-    else:
-        form = NewsletterSignupForm()
 
-    context = {
-        'form': form,
-    }
-    return render(request, 'home/index.html', context)
+    return render(request, 'home/index.html')
